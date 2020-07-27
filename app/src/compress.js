64cda4c78ea4845b911e0fe2f67abcf320a4a1d0
machman5 <jay.yost1@gmail.com>
2020-07-27 01:30:47 -0500

    Update compress.js

--- app/src/compress.js
+++ app/src/compress.js
@@ -9,6 +9,7 @@ function compress(req, res, input) {
             quality: req.params.quality,
             progressive: true,
             optimizeScans: true
+            reductionEffort: 2
         })
         .toBuffer((err, output, info) => {
             if (err || !info || res.headersSent) return redirect(req, res);
